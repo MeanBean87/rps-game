5b7b7db6ac3853b53b08ab45fa9e077d2601b41f
unknown <samtisme@gmail.com>
2023-06-29 12:12:43 -0700

    Put java code in for alerts

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,18 @@ function playGame() {
   let userChoice = prompt(
     "Let's play Rock, Paper, Scissors! Please choose R, P, or S to begin."
   );
+  const choices = {
+   "R": "Rock",
+    "P": "Paper",
+    "S": "Scissors",
+  };
 
-    let choices = ["R", "P", "S"];
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
     if (userChoice === computerChoice) {
         result = "tie";
         gameStats.ties++;
+        alert(`you chose ${userChoice}, computer chose ${computerChoice}, it's a tie`)
     } else if (
         (userChoice === "R" && computerChoice === "S") ||
         (userChoice === "P" && computerChoice === "R") ||
@@ -22,8 +27,12 @@ function playGame() {
     ) {
         result = "win";
         gameStats.wins++;
+        alert(`you chose ${userChoice}, computer chose ${computerChoice}, it's a win`)
     } else {
         result = "loss";
         gameStats.losses++;
+        alert(`you chose ${userChoice}, computer chose ${computerChoice}, it's a loss`)
     }
+    alert(`wins: ${gameStats.wins}\n losses: ${gameStats.losses}\n ties: ${gameStats.ties}`)
 }
+
