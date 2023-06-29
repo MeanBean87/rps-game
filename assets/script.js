e23dd676befa8f12d041ca0dd6ec81a4dab843fb
Michael Mattingly <mikeamatt1@hotmail.com>
2023-06-29 15:41:38 -0400

    Added replay confirmation and fixed output on alerts.

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,61 @@
 const gameStats = {
-    wins: 0,
-    losses: 0,
-    ties: 0,
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+
+function playGame() {
+  let userChoice = prompt(
+    "Let's play Rock, Paper, Scissors! Please choose R, P, or S to begin."
+  );
+
+  const choices = {
+    R: "Rock",
+    P: "Paper",
+    S: "Scissors",
   };
-  
-  function playGame() {
-    let userChoice = prompt(
-      "Let's play Rock, Paper, Scissors! Please choose R, P, or S to begin."
+
+  userChoice = choices[userChoice.toUpperCase()];
+
+  let computerChoice =
+    choices[
+      Object.keys(choices)[
+        Math.floor(Math.random() * Object.keys(choices).length)
+      ]
+    ];
+
+  if (userChoice === computerChoice) {
+    result = "tie";
+    gameStats.ties++;
+    alert(
+      `you chose ${userChoice}, computer chose ${computerChoice}, it's a tie`
     );
-    const choices = {
-      R: "Rock",
-      P: "Paper",
-      S: "Scissors",
-    };
-  
-    let computerChoice =
-      choices[Object.keys(choices)[Math.floor(Math.random() * Object.keys(choices).length)]];
-  
-    if (userChoice === computerChoice) {
-      result = "tie";
-      gameStats.ties++;
-      alert(`You chose ${userChoice}, computer chose ${computerChoice}. It's a tie.`)
-    } else if (
-      (userChoice === "R" && computerChoice === "S") ||
-      (userChoice === "P" && computerChoice === "R") ||
-      (userChoice === "S" && computerChoice === "P")
-    ) {
-      result = "win";
-      gameStats.wins++;
-      alert(`You chose ${userChoice}, computer chose ${computerChoice}. You win!`);
-    } else {
-      result = "loss";
-      gameStats.losses++;
-      alert(`You chose ${userChoice}, computer chose ${computerChoice}. You lose!`);
-    }
-    
-    alert(`Wins: ${gameStats.wins}\nLosses: ${gameStats.losses}\nTies: ${gameStats.ties}`);
+  } else if (
+    (userChoice === "Rock" && computerChoice === "Scissors") ||
+    (userChoice === "Paper" && computerChoice === "Rock") ||
+    (userChoice === "Scissors" && computerChoice === "Paper")
+  ) {
+    result = "win";
+    gameStats.wins++;
+    alert(
+      `you chose ${userChoice}, computer chose ${computerChoice}, it's a win`
+    );
+  } else {
+    result = "loss";
+    gameStats.losses++;
+    alert(
+      `you chose ${userChoice}, computer chose ${computerChoice}, it's a loss`
+    );
   }
-  
+  alert(
+    `wins: ${gameStats.wins}\nlosses: ${gameStats.losses}\nties: ${gameStats.ties}`
+  );
+
+  if (
+    confirm(
+      "Would you like to play again?\nPress OK to play again.\nPress Cancel to quit."
+    )
+  ) {
+    playGame();
+  }
+}
